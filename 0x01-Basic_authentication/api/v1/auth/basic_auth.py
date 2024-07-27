5d6de8f932ee0a51448cfe4a6bcad2a837bb8122
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-27 11:58:00 +0300

    task 6 creation of Basic_aut class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+created on Sat Jul 11:55 2024
+@Author: James Gatheru
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Authentication class
+    """
+    pass
