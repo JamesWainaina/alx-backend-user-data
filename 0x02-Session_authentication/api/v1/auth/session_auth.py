a3703573333e89f3335d7d3e8f6176cc1865bef7
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-28 15:22:34 +0300

    creation of sessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+created on July 28 2024
+Author @ James Gatheru
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session Auth class
+    """
+    pass
