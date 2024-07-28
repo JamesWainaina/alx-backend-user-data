eb583120e6a4130fd22373146478a5d858225c9d
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-28 15:07:41 +0300

    fixing pycodestyle in users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -33,7 +33,7 @@ def view_one_user(user_id: str = None) -> str:
 
     if user_id == 'me' and request.current_user:
         return jsonify(request.currrent_user.to_json())
-    
+
     user = User.get(user_id)
 
     if user is None:
@@ -129,11 +129,12 @@ def update_user(user_id: str = None) -> str:
     user.save()
     return jsonify(user.to_json()), 200
 
+
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def get_authenticated_user() -> str:
     """ Get the authenticated User
     """
     if not request.current_user:
         abort(404)
-    
+
     return jsonify(request.current_user.to_json())
